f9ca3f2db40e5cdfc1e946ad1cf00eb3dd108126
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-28 17:58:42 -0700

    passed all tests

--- main.js
+++ main.js
@@ -2,6 +2,70 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin(user) {
+    if (user.userRole === 'ADMIN') {
+        return true;
+    } else  {
+        return false;
+    }
+}
+
+function getEmail(user) {
+
+    return user.firstName.toLowerCase() + "." + user.lastName.toLowerCase() + "@codeimmersives.com"
+    
+}
+
+function getPlaylistLength(playlist) {
+    return playlist.songs.length;
+}
+
+function getHardestHomework(homework) {
+    
+    let empty = ''
+
+    if (homework.length === 0) {
+        return empty
+    }
+    
+    // variables for for loop have to be after if startment for empty array due to "scope"
+    let lowestName = homework[0].name
+    let lowest = homework[0].averageScore
+
+    for (let i = 0; i < homework.length; i++) {
+        
+        
+        if (homework[i].averageScore < lowest) {
+            lowest = homework[i].averageScore
+            lowestName = homework[i].name
+            
+        }
+        
+    
+    }
+    return lowestName
+    
+}
+//   array of names, array of phone numbers, new array of objects called phonebook
+function createPhonebook(namesArr, phoneNumbersArr, phonebook = {}) {
+    
+    // let phonebook = {
+    //     names: phoneNumbers
+    // }
+
+    for (let i = 0; i < namesArr.length; i++) {
+        
+        let names = namesArr[i]
+        let phoneNumbers = phoneNumbersArr[i]
+        phonebook[names] = phoneNumbers
+
+        
+    }
+
+
+    return phonebook
+}
+
 
 
 // ┌─────────────────────────────────────┐
